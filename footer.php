c80bfb9f681217d274cccd01553df63ae1a8f8af
MaximePolese <143593018+MaximePolese@users.noreply.github.com>
2023-09-07 11:58:01 +0200

    premier commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+</main>
+<footer>
+    <div class="d-flex justify-content-between bg-info">
+        <div class="d-flex py-3">
+            <div class="px-2">© 2023 Copyright: Le Campus Numérique</div>
+            <a class="px-2" href="#">Conditions générales</a>
+            <a class="px-2" href="#">Mentions légales</a>
+            <a class="px-2" href="#">Politique de confidentialité</a>
+            <a class="px-2" href="#">Gestion des cookies</a>
+        </div>
+        <div class="d-flex align-self-center">
+            <a class="px-2" href="http://www.facebook.fr"><img src="icons/facebook.png" alt="facebook"
+                                                               width="30"></a>
+            <a href="http://www.instagram.fr"><img src="icons/instagram.png" alt="instagram" width="30"></a>
+            <a class="px-2" href="http://www.tiktok.fr"><img src="icons/tiktok.png" alt="tiktok" width="30"></a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ simple-catalog.php
@@ -0,0 +1,45 @@
+<?php
+
+$products = ["Sacs de randonnée", "Tentes", "Matelas", "Sacs de couchage", "Bâtons"];
+
+////afficher le tableau
+//print_r($products);
+//echo "<br>";
+//echo "<br>";
+//
+////Classer dans l'ordre croissant alphabétique
+//echo sort($products);
+//echo "<br>";
+//
+////boucle while
+//$i = 0;
+//while ($i <= 4) {
+//    echo $products[$i] . "<br>";
+//    $i++;
+//}
+//echo "<br>";
+//
+////boucle do while
+//$i = 0;
+//do {
+//    echo $products[$i] . "<br>";
+//    $i++;
+//} while ($i <= 4);
+//echo "<br>";
+//
+////boucle for
+//for ($i = 0; $i < count($products); $i++) {
+//    echo $products[$i] . "<br>";
+//}
+//echo "<br>";
+//
+////boucle foreach
+//foreach ($products as $product) {
+//    echo $product . "<br>";
+//}
+//echo "<br>";
+//
+//echo $products[0] . "<br>";
+//echo $products[4];
+//
+
