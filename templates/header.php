8341bac9703fa6bcd642880c64fe38a105ae46f7
MaximePolese <143593018+MaximePolese@users.noreply.github.com>
2023-09-07 15:05:41 +0200

    remise en forme terminé

--- templates/header.php
+++ templates/header.php
@@ -5,10 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../CSS/reset.css">
+    <link rel="stylesheet" href="CSS/reset.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="CSS/style.css">
     <title>Rando Shop</title>
 </head>
 <body>
